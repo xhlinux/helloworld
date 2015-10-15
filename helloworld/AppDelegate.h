2543186b3a58b02c7f34780c3317037467309d0b
apple <apple@xiaoheng-macbook-air.local>
2015-10-15 17:15:59 +0800

    Initial Commit

---
+++ helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by xiaoya on 15/10/15.
+//  Copyright © 2015年 com.xiaoheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloworld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by xiaoya on 15/10/15.
+//  Copyright © 2015年 com.xiaoheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
